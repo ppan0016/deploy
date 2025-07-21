83142bcc412656f224fc503cdc295d08d2744e37
Pooja Pancholi <pooja.pancholi@deakin.edu.au>
2025-07-21 18:52:05 +1000

    week 3 work

---
+++ week3/first-app/prac1/src/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LogoImage(){
+  return (
+    <img src="./logo192.png" alt="Basic Logo"/>
+  )
+}
+
+export default LogoImage
---
+++ week3/first-app/prac1/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import LogoImage from './Image'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <div>
+    <h1>This is a heading!</h1>
+    <p>This is a para!</p>
+  </div>
+);
+
+const style = {
+  color: 'red',
+  fontSize: '30px' 
+}
+
+const datediv = ReactDOM.createRoot(document.getElementById('newdate'));
+
+const trimester = "T2";
+var d = new Date();
+const year = d.getFullYear();
+
+// datediv.render(
+//   <h3 style={style}>
+//     The current Trimester is : {trimester} {year}
+//   </h3>
+// );
+
+// component
+datediv.render(
+ <HeaderComponent />
+);
+
+
+const logo = ReactDOM.createRoot(document.getElementById('logo'));
+
+logo.render(
+  <div>
+    {/* <img src="./logo192.png" alt="Basic Logo"/> */}
+    <LogoImage /> 
+     {/* <img src="./logo512.png" alt="Basic Logo"/> */}
+  </div>
+);
+
+// //creating a component
+function HeaderComponent(){
+  return(<h3 style={style}>
+    The current Trimester is : {trimester} {year}
+  </h3>)
+}
