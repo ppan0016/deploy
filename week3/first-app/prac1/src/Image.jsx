2d0ef5e34521c6434bdd83c11dcbd03713d8e0a5
ppan0016 <118229265+ppan0016@users.noreply.github.com>
2025-09-10 15:34:32 +1000

    Delete week3 directory

--- week3/first-app/prac1/src/Image.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function LogoImage(){
-  return (
-    <img src="./logo192.png" alt="Basic Logo"/>
-  )
-}
-
-export default LogoImage
--- week3/first-app/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-
-ReactDOM.render(<h1> Welcome to SIT313 </h1>, document.getElementById('root') )
